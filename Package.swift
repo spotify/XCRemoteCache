5f2f0dad0750eb568c91f8897f8eb47f09648c34
ramen2020 <vfvsfv.dlk22@gmail.com>
2022-01-27 20:49:38 +0900

    Upgrade swift-argument-parser to a stable version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/marmelroy/Zip.git", from: "2.0.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "3.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
         .package(url: "https://github.com/tuist/XcodeProj.git", from: "8.0.0"),
     ],
     targets: [
