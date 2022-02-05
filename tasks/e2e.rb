c6b31d30864dd50b3643c6f541817e7863ae6116
Bartosz Polaczyk <polac24@gmail.com>
2022-02-05 21:31:57 +0100

    Disable exclusive DD paths

--- tasks/e2e.rb
+++ tasks/e2e.rb
@@ -179,7 +179,8 @@ namespace :e2e do
         dump_podfile(consumer_configuration, template_path)
         puts('Building consumer ...')
         Dir.chdir(E2E_COCOAPODS_SAMPLE_DIR) do
-            build_project({'derivedDataPath' => "#{DERIVED_DATA_PATH}_Consumer"})
+            # TODO: Change DerivedData's path to emulate multi-machines setup. Blocked by #59
+            build_project({'derivedDataPath' => "#{DERIVED_DATA_PATH}"})
             valide_hit_rate
         end
     end
