e263cb6e25f5c09663b18a6b8d7fec78f798cfc7
Vasily Fedorov <vasily.fedorov@alibaba-inc.com>
2022-02-09 17:01:35 +0300

    fix comment

--- cocoapods-plugin/lib/cocoapods-xcremotecache/command/hooks.rb
+++ cocoapods-plugin/lib/cocoapods-xcremotecache/command/hooks.rb
@@ -145,7 +145,7 @@ module CocoapodsXCRemoteCacheModifier
           ]
           prebuild_script.dependency_file = "$(TARGET_TEMP_DIR)/prebuild.d"
 
-          # Move prebuild (last element) to the first position (to make it real 'prebuild')
+          # Move prebuild (last element) to the position before compile sources phase (to make it real 'prebuild')
           compile_phase_index = target.build_phases.index(target.source_build_phase)
           target.build_phases.insert(compile_phase_index, target.build_phases.delete(prebuild_script))
         elsif mode == 'producer' || mode == 'producer-fast'
