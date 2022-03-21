b5ff16484fefafb26de2c2fe4d3c6a26730d24c3
alecgarza96 <43724421+alecgarza96@users.noreply.github.com>
2022-03-21 11:31:09 -0500

    removed trailing whitespace

--- Sources/XCRemoteCache/Commands/Prepare/CCWrapperBuilder.swift
+++ Sources/XCRemoteCache/Commands/Prepare/CCWrapperBuilder.swift
@@ -438,7 +438,7 @@ class TemplateBasedCCWrapperBuilder: CCWrapperBuilder {
                     isSuffixed(argv[i],".c++") ||
                     isSuffixed(argv[i],".cxx") ||
                     isSuffixed(argv[i],".S") ||
-                    isSuffixed(argv[i],".s") 
+                    isSuffixed(argv[i],".s")
                 ) {
                     // a full list of extensions is taken from https://clang.llvm.org/docs/ClangFormatStyleOptions.html
                     // support for .m,.mm,.c,.cc,.cpp,.c++,.cxx input files
