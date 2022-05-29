49854e6e2d6373dfad942d120803751389076d95
Bartosz Polaczyk <polac24@gmail.com>
2022-05-29 16:06:29 +0200

    Add Sample standalone app

---
+++ e2eTests/StandaloneSampleApp/StandaloneApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StandaloneApp
+//
+//  Created by Bartosz Polaczyk on 2022-05-29.
+//
+
+import UIKit
+import MixedTarget
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
