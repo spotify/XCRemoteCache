de24e609ef543824332ff89e66c1d497baf6fac0
Bartosz Polaczyk <polac24@gmail.com>
2023-03-11 10:34:24 -0500

    Delete leftover comment

--- Sources/XCRemoteCache/Commands/Libtool/XCCreateUniversalBinary.swift
+++ Sources/XCRemoteCache/Commands/Libtool/XCCreateUniversalBinary.swift
@@ -52,7 +52,6 @@ class XCCreateUniversalBinary: XCLibtoolLogic {
             .deletingLastPathComponent()
             .deletingLastPathComponent()
             .deletingLastPathComponent()
-        errorLog("\(tempDir.absoluteString)")
         self.firstInputURL = firstInputURL
         self.toolName = toolName
         self.fallbackCommand = fallbackCommand
