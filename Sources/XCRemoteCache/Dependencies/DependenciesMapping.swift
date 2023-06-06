aa6b014f9dfde2357e810eec20d79c34c4f9d828
Bartosz Polaczyk <polac24@gmail.com>
2023-06-05 21:26:43 -0700

    Add Generated Assets in Xcode 15 beta support

--- Sources/XCRemoteCache/Dependencies/DependenciesMapping.swift
+++ Sources/XCRemoteCache/Dependencies/DependenciesMapping.swift
@@ -22,5 +22,5 @@ import Foundation
 enum DependenciesMapping {
     /// Specifies which ENVs should be rewritten in the dependencies generation to make generic (paths agnostics)
     /// list of dependencies
-    static let rewrittenEnvs = ["BUILD_DIR", "SRCROOT"]
+    static let rewrittenEnvs = ["DERIVED_FILE_DIR", "BUILD_DIR", "SRCROOT"]
 }
